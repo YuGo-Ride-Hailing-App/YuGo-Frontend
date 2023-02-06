4aea9de59f7062ddc88f0b4473ffca3a4756322a
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2023-02-06 16:12:19 +0100

    Polyfills error fixed

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -53,4 +53,4 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-(window as any)['global'] = window;
+(window as any).global = window
