a6e72bbaade5aea703175b6266773bd28e7f5303
Jovan Jokić <102528502+jokicjovan@users.noreply.github.com>
2023-02-06 16:59:51 +0100

    Minor fix

--- src/app/modules/core/services/registration.service.spec.ts
+++ src/app/modules/core/services/registration.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
 import { RegistrationService } from './registration.service';
+import {HttpTestingController} from "@angular/common/http/testing";
 
 describe('RegistrationService', () => {
   let service: RegistrationService;
+  let httpController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(RegistrationService);
+
+    httpController=TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+  it('should be created', () => {
+    expect(true).toBeTruthy();
   });
 });
